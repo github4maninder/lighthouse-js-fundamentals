87db6450fe1a6e6c329a18f46a4187f95d1775ad
github4maninder <maninder.email@gmail.com>
2021-09-30 23:57:54 -0400

    use of chorus for loop in js

--- chorus-for.js
+++ chorus-for.js
@@ -3,3 +3,20 @@ for (let repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
+
+var x = 100;
+
+while (x <= 200) {
+  if (x % 3 === 0 && x % 4 === 0) {
+    console.log("LoopyLighthouse");
+    }else if(x % 4 === 0){
+    console.log("Lighthouse");
+    }else if (x % 3 === 0) {
+    console.log("Loopy");
+    }
+    else {
+    console.log(x);
+    }
+    // increment x at the end of each loop
+    x = x + 1 ; 
+}
